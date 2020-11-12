6d48445647dfac34b15c90ca62b2fbf78dcb326d
Gaston Kofmansky <42493523+Gastkof@users.noreply.github.com>
2020-11-12 14:57:32 +0200

    Update php-q1.php

--- php/php-q1.php
+++ php/php-q1.php
@@ -2,3 +2,16 @@
 For example: calling unique_names([‘Dan’,’Ron’,’Boris], [‘Boris’,’Dan’, Ruth’]) should return:
 
 [‘Dan’,’Ron’,’Boris,’Ruth’] in any order.
+
+
+<?php
+function  unique_names(array $names1 ,array $names2){
+
+$arr = array();
+array_push($arr, array_unique(array_merge( $names1 , $names2 )));
+
+return $arr;
+
+}
+print_r( unique_names(['Dan','Ron','Boris'], ['Boris','Dan', 'Ruth']));
+?> 
