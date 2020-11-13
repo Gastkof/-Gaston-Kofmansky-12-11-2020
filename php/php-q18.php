dd02f82a7c4edbb5f95744adbb9c077e5572b109
Gaston Kofmansky <42493523+Gastkof@users.noreply.github.com>
2020-11-13 15:21:45 +0200

    Create php-q18.php
    ans

---
+++ php/php-q18.php
@@ -0,0 +1,5 @@
+Question p18 - Submission file: php-q18.php
+What is the function file_get_contents() useful for?
+
+reads a file into a string.
+adn the best wey to read the contents of a file into a string
