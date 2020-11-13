504b7b16eb0d1e8f717d28caa5dbcd7866813aec
Gaston Kofmansky <42493523+Gastkof@users.noreply.github.com>
2020-11-13 16:19:01 +0200

    Update php-q13.php
    ans

--- php/php-q13.php
+++ php/php-q13.php
@@ -1,1 +1,59 @@
-#unieq_names
+Suppose that you have to implement a class named Dragonball.
+This class must have an attribute named ballCount (which starts from 0) and a method iFoundaBall.
+When iFoundaBall is called, ballCount is increased by one. 
+If the value of ballCount is equal to seven, then the message You can ask your wish is printed, 
+and ballCount is reset to 0. How would you implement this class?
+
+
+
+<?php
+class DragonBall {
+  // Properties
+  public $ballCount;
+
+
+  function __construct(){
+  	
+   	$this->set_ballCount(0);
+  }
+
+
+  // Methods
+ public function set_ballCount($ballCount) {
+    $this->ballCount = $ballCount;
+  }
+  function get_ballCount()  {
+    return $this->ballCount;
+    
+  }
+ public function iFoundaBall(){
+  			if($this->ballCount<7){
+          
+            	$this->ballCount+=1;}
+          else  {
+      
+          echo "You can ask your wish" ."</br>";
+          
+   $this->ballCount=0;
+  }
+  }
+
+  
+}
+
+$Dragon = new DragonBall();
+
+$Dragon->iFoundaBall();
+$Dragon->iFoundaBall();
+$Dragon->iFoundaBall();
+$Dragon->iFoundaBall();
+$Dragon->iFoundaBall();
+$Dragon->iFoundaBall();
+$Dragon->iFoundaBall();
+$Dragon->iFoundaBall();
+
+$Dragon->iFoundaBall();
+$Dragon->iFoundaBall();
+
+?>
+ 
