eba43aa1bd73579352a08f85b18de6ccdf7227e0
Gaston Kofmansky <42493523+Gastkof@users.noreply.github.com>
2020-11-13 15:23:56 +0200

    Update php-q9.php

--- php/php-q9.php
+++ php/php-q9.php
@@ -1,1 +1,6 @@
-#unieq_names
+Question p10 - Submission file: php-q9.php
+What is the method to register a variable into a session?
+
+session_register ( mixed $name , mixed ...$names ) : bool
+
+$_SESSION[""]
