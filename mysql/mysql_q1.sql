32de0922688f42770672c88fc4e03af9034aaad0
Gaston Kofmansky <42493523+Gastkof@users.noreply.github.com>
2020-11-13 11:14:40 +0200

    Update mysql_q1.sql
    ans

--- mysql/mysql_q1.sql
+++ mysql/mysql_q1.sql
@@ -2,3 +2,7 @@ TABLE employees
    id INTEGER NOT NULL PRIMARY KEY
    mgrId INTEGER REFERENCES employees (id)
    name VARCHAR (30) NOT NULL
+
+
+SELECT e.name, m.name 
+FROM employees e FULL OUTER JOIN employees m on e.id =m.mgrId
