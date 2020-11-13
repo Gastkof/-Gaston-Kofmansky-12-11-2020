aa6534d4bb777e9001acfc727b2de10f77f2dacf
Gaston Kofmansky <42493523+Gastkof@users.noreply.github.com>
2020-11-13 14:36:14 +0200

    Update php-q6.php

--- php/php-q6.php
+++ php/php-q6.php
@@ -1,1 +1,4 @@
-#unieq_names
+Question p7 - Submission file: php-q6.php
+Explain setcookie() function in PHP?
+ans:
+a function that can be define a cookie to be send along withr the rest of the HHTP headers.
