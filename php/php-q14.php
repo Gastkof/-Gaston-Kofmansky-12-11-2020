f217227874d3b55aee5b3411ab5bbb405ba0edec
Gaston Kofmansky <42493523+Gastkof@users.noreply.github.com>
2020-11-13 15:17:05 +0200

    Update php-q14.php

--- php/php-q14.php
+++ php/php-q14.php
@@ -1,1 +1,15 @@
-#unieq_names
+Question p19 - Submission file: php-q14.php
+How can we access the data sent through the URL with the GET method?
+
+
+$_GET is an array of variables passed to the current script via the URL parameters.
+
+GET may be used for sending non-sensitive data.
+
+<a href="test_get.php?subject=PHP&web=check.com">Test $GET</a>
+
+in the test_get.php :
+
+<?php
+echo "Study " . $_GET['subject'] . " at " . $_GET['web'];
+?>
