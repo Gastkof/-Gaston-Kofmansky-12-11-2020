4f169269c15cdc9f93bd5d09ecee8b764a2a365a
Gaston Kofmansky <42493523+Gastkof@users.noreply.github.com>
2020-11-13 14:42:42 +0200

    Update php-q12.php

--- php/php-q12.php
+++ php/php-q12.php
@@ -1,1 +1,6 @@
-#unieq_names
+Question p16 - Submission file: php-q12.php
+What is htaccess in PHP?
+
+ans:
+htaccess  file is a distributed server configuration file. 
+The htaccess file allows you to set server configurations for a specific directory.
