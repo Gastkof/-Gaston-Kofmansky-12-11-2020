d3edf63300dfea7c1e7dcfa2e62f37805ec12b3d
Gaston Kofmansky <42493523+Gastkof@users.noreply.github.com>
2020-11-13 14:57:34 +0200

    Update php-q4.php
    ans

--- php/php-q4.php
+++ php/php-q4.php
@@ -1,15 +1,12 @@
-#Array Sum
-    <?php
+ <?php
 
-    function  prependSum (array  $array)  :  int
-    {
-           $sum = array_sum( $array ) ;
-            array_unshift ($array, $sum) ;
-
-            return  $sum;
-    }
- 
-  $array = array (1 , 2, 3 ) ;
-  filterNumbersFromArray ($arr) ;
-  echo ( $sum.PHP_EOL );
-  print_r ( array_values ( $arr ) ) ;
+function prependSum (array &$array) : int {
+ $sum = array_sum( $array );
+echo array_sum($sum);
+array_unshift ($array, $sum);
+ return $sum;
+ }
+ $array = array (1 , 2, 3 ) ; prependSum($array) ;
+ echo($sum.PHP_EOL);
+ print_r( array_values ( $array ) ) ;
+?>
