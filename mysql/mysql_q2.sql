07b0d491ab641e543c1368d1464e787d342300ca
Gaston Kofmansky <42493523+Gastkof@users.noreply.github.com>
2020-11-13 11:27:05 +0200

    Update mysql_q2.sql
    ans

--- mysql/mysql_q2.sql
+++ mysql/mysql_q2.sql
@@ -6,3 +6,9 @@ TABLE fsib
    companyName VARCHAR (30) NOT NULL PRIMARY KEY
    sharePrice FLOAT NOT NULL
    shareOutstanding  INTEGER NOT NULL
+   
+   
+  select fa.companyName, fa.marketCapitalization
+  from fsia fa join fb on fb.companyName=fb.companyName
+  where marketCapitalization = (fb.sharePrice * fb.shareOutstanding)
+  order by marketCapitalization desc
