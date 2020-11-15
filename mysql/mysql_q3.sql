311332bc8ecc9e81a64477fef518c2193ba95bcf
Gaston Kofmansky <42493523+Gastkof@users.noreply.github.com>
2020-11-15 11:28:15 +0200

    Update mysql_q3.sql

--- mysql/mysql_q3.sql
+++ mysql/mysql_q3.sql
@@ -3,6 +3,12 @@ id INTEGER PRIMARY KEY,
 name VARCHAR (30) NOT NULL,
 country VARCHAR (30) NOT NULL
 
+o1:
 select distinct country
 from companies 
 order by country 
+
+o2:
+select distinct country,name
+from companies 
+order by country
