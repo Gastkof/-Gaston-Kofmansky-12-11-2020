37b7f9a4815c7bcc06ecf01adc091183886297bb
Gaston Kofmansky <42493523+Gastkof@users.noreply.github.com>
2020-11-15 08:38:37 +0200

    Update php-q10.php

--- php/php-q10.php
+++ php/php-q10.php
@@ -2,6 +2,6 @@ Question p11 - Submission file: php-q10.php
 Write syntax to open a file in PHP?
 
 ans:
-$myfile = fopen("webdictionary.txt", "r") or die("Unable to open file!");
-echo fread($myfile,filesize("webdictionary.txt"));
+$myfile = fopen("file.txt", "r") or die("Unable to open file!");
+echo fread($myfile,filesize("file.txt"));
 fclose($myfile);
